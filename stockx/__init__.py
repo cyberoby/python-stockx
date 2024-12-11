d9285ca14426aadcdb981384e59002f63593ce26
Roberto Scifo <roberto@naha.it>
2024-12-11 13:35:25 +0100

    added changelog

--- stockx/__init__.py
+++ stockx/__init__.py
@@ -4,6 +4,8 @@ from stockx.api import StockX, StockXAPIClient
 from stockx.logging import configure_logging, logger
 from stockx.models import *
 
+__version__ = '0.1.0'
+
 __all__ = (
     'StockX',
     'StockXAPIClient',
